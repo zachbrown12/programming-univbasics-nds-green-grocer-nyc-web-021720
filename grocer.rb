10ee6f667432f5c5de635997f37a8a29cfbecea0
Zach Brown <zachmb22@gmail.com>
2020-02-11 03:00:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,13 +57,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  # code here
-  cart.keys.each do |item|
-    if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price]* 0.8).round(2)
-    end
-  end
-  cart
+  counter = 0
 end
 
 def checkout(cart, coupons)
