8c9375eb64b7af1ec03f1e84e3ad40fd9481acec
Zach Brown <zachmb22@gmail.com>
2020-02-11 02:57:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,8 +34,10 @@ def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name = "#{coupons[counter][:num]
-  
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      cart_item_with_coupon[:count] += coupons
 end
 
 def apply_clearance(cart)
