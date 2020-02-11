ea845790054c47f9c33135a2fc641522f27170c6
Zach Brown <zachmb22@gmail.com>
2020-02-11 02:41:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,5 +51,11 @@ end
 
 def checkout(cart, coupons)
    cart = consolidate_cart(cart: cart)
-  
+  cart = apply_coupons(cart: cart, coupons: coupons)
+  cart = apply_clearance(cart: cart)
+  result = 0
+  cart.each do |food, info|
+    result += (info[:price] * info[:count]).to_f
+  end
+  result > 100 ? result * 0.9 : result
 end
