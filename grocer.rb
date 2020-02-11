512e8641220545ef28239fecd5538d02c1aa082b
Zach Brown <zachmb22@gmail.com>
2020-02-11 02:46:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,3 +1,13 @@
+def find_item_by_name_in_collection(name, collection)
+  counter = 0
+  while counter < collection.length
+   if collection[counter][:item] ==name
+     return collection[counter]
+    end
+    counter += 1
+  end
+end
+
 def consolidate_cart(cart)
   # code here
   consolidated = {}
