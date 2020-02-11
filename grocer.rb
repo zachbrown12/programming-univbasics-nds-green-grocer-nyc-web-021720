d179bfe9a7a261af08fd460ac139c558cf9cdc45
Zach Brown <zachmb22@gmail.com>
2020-02-11 03:10:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -68,7 +68,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  consolidated_cart = consolidated_cart(cart)
+  consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   
